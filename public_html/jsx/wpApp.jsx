9c4ecf522fbee00a8296d6e515e4e414ac6041d3
kjr63 <kaj.risberg@helsinkinet.fi>
2021-05-07 14:27:49 +0300

    lisätty thinking-react esimerkki

--- public_html/jsx/wpApp.jsx
+++ public_html/jsx/wpApp.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import Header from './components/header.jsx';
 import Content from './components/content.jsx';
+import FilterableProductTable from './components/thinking-react-example.jsx';
 import '../firebase/firebase.jsx';
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 export default class WpApp extends React.Component {
     constructor (props) {
@@ -16,6 +26,7 @@ export default class WpApp extends React.Component {
             <div className="template">
                 <Header />
                 <Content />
+				<FilterableProductTable products={PRODUCTS} />				
             </div>
         );
     }
